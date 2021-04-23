48a6725970ea8baa22845d16991ae246dc569dbc
ohletamyleta <ohletamyleta@gmail.com>
2021-04-23 10:55:32 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => alert("no! don't click!")} />
   </div>,
   document.getElementById('root')
 );
